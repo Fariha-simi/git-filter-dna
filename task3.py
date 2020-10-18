dc3e1c20892ce05794c497238df5c2a0e9572506
Fariha-simi <farihatasnimsimi.physics@gmail.com>
2020-10-18 06:30:04 -0700

    first commit

---
+++ task3.py
@@ -0,0 +1,33 @@
+"""
+Let's make some horrible operation in people's DNA
+"""
+user_dna_sequence = [
+[
+"Roksana Kozlova", 18, "GTTAGCCGACTTGGCXCTTT"
+],
+[
+"Roza Kiseleva", 19, "ATTTGGCGAATTGGCCTTT"
+],
+[
+"Kliment Volkov", 27, "ATTTGACCAATTGZGCAAA"
+],
+[
+"Afanas Morozov", 22, "ATTTGACCGATTNGGCAA"
+],
+]
+
+dna_elements = ["A", "T", "G", "C"]
+arr = []
+for element in user_dna_sequence :
+    if element[1] > 20 :
+        arr.append(element)
+print(arr)
+
+for i in arr[0][2] :
+    arr[0][2] = i.replace("A","G")
+    print(arr[0][2], end = "\n")
+        
+for i in arr[1][2] :
+    arr[1][2] = i.replace("A","G")
+    print(arr[1][2], end = "\n")    
+    
