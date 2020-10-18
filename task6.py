6ab7ce78a504c0e1884e59df08ea403a753e7ed7
Fariha-simi <farihatasnimsimi.physics@gmail.com>
2020-10-18 11:24:11 -0700

    first commit

--- task6.py
+++ task6.py
@@ -23,7 +23,49 @@ arr.append(user_dna_sequence[0][2])
 arr.append(user_dna_sequence[1][2])
 arr.append(user_dna_sequence[2][2])
 arr.append(user_dna_sequence[3][2])
-print(arr)
-print(arr[0])
-if (len(arr[0])-1) == dna_elements[0]:
+#print(arr)
+
+arr1 = []
+for i in arr[0] :
+    arr1.append(i)
+#print(arr1)
+arr1.reverse()
+#print(arr1)
+if arr1[0] == dna_elements[0] :
     print(arr[0])
+else :
+    print("Last element is not A.")
+
+arr2 = []
+for i in arr[1] :
+    arr2.append(i)
+#print(arr2)
+arr2.reverse()
+#print(arr2)
+if arr2[0] == dna_elements[0] :
+    print(arr[1])
+else :
+    print("Last element is not A.")
+
+arr3 = []
+for i in arr[2] :
+    arr3.append(i)
+#print(arr3)
+arr3.reverse()
+#print(arr3)
+if arr3[0] == dna_elements[0] :
+    print(arr[2])
+else :
+    print("Last element is not A.")
+
+arr4 = []
+for i in arr[3] :
+    arr4.append(i)
+#print(arr4)
+arr4.reverse()
+#print(arr4)
+if arr4[0] == dna_elements[0] :
+    print(arr[3])
+else :
+    print("Last element is not A.")
+
